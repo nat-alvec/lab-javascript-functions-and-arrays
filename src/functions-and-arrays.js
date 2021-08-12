8b8839426f2f1432f0d2e254e10295b30c3fd064
Natália Alves <natalia.alvescam@gmail.com>
2021-08-11 21:57:20 -0300

    done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,19 +1,33 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(numb1, numb2){
+  return Math.max(numb1,numb2)
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(){
+  let LongestWord = ""
+  
+  for ( let i = 0; i < words.length; i++){
+    if( words[i].length > LongestWord.length ){
+      LongestWord = words[i]
+    }
+  }  
+    return LongestWord
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers() {
+  const sum = (num1, num2) => num1 + num2
+  return(listNumbers.reduce(sum))
+}
 
 
 
